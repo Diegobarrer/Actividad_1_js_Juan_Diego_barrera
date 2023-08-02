9db38c19ad1132f26dc6f6a00169f15584fa0ffd
Diegobarrer <barrerajd07@gmail.com>
2023-08-01 19:24:48 -0500

    Entregable_js

---
+++ Index2.js
@@ -0,0 +1,9 @@
+let name = "Juan Diego"
+let Lastname = "Barrera Serna"
+let age = 21
+const birthDate = "01/05/2002"
+let city = "Medellin"
+
+
+console. log(
+    "Mi nombre es", name, Lastname, "Tengo", age, "Años", "naci el", birthDate, "y soy de", city)
